26843862d320f79beaeacac5949682cd4c9c011b
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-12-09 11:43:50 -0300

    board and cell init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import Board from "./components/Board"
 
+const board = Array(20).fill(null).map(() => Array(12).fill('empty'));
 
 function App() {
 
   return (
 
-    <div>
-
+    <div className="App">
+      <h1>Tetris</h1>
+      <Board currentBoard={board} />
     </div>
 
   )
