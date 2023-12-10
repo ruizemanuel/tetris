b24f897b2cee635851e6787345a413080c874c36
Emanuel Ruiz <ruiz.e.642@gmail.com>
2023-12-10 17:06:44 -0300

    changed start game style button - score style and next blocks style

--- src/App.jsx
+++ src/App.jsx
@@ -17,18 +17,25 @@ function App() {
       <h1>Tetris</h1>
       <Board currentBoard={board} />
       <div className="controls">
-        <h2>Score: {score}</h2>
-        {isPlaying ? (<UpcomingBlocks upcomingBlocks={upcomingBlocks} />) : (
-          <button onClick={startGame}>New Game</button>
+        <div className="score">
+          <h2 className="title">Score</h2>
+          <h2 className="number">{score}</h2>
+        </div>
+        {isPlaying ? (<div className="blocks-container">
+          <h2>Next Blocks</h2>
+          <UpcomingBlocks upcomingBlocks={upcomingBlocks} />
+        </div> ) : (
+          <button className="start-btn" onClick={startGame}>START</button>
         )}
+        {isPlaying && <button className="play-btn" onClick={() => setIsPaused((prevIsPaused) => !prevIsPaused)}>
+          {isPaused ? (
+            <img src={play} alt="Play" width="100%" height="100%" />
+          ) : (
+            <img src={pause} alt="Pause" width="100%" height="100%" />
+          )}
+        </button>}
       </div>
-      {isPlaying && <button className="play-btn" onClick={() => setIsPaused((prevIsPaused) => !prevIsPaused)}>
-        {isPaused ? (
-          <img src={play} alt="Play" width="100%" height="100%"  />
-        ) : (
-          <img src={pause} alt="Pause" width="100%" height="100%" />
-        )}
-      </button>}
+
 
     </div>
 
